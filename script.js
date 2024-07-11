33fe524f78d97bc7b88a2decab6866ff424dcc62
Gabriel Monteiro <gabrielm.reserva@gmail.com>
2024-07-10 23:50:53 -0300

    adicionado biblioteca de terceiros

--- script.js
+++ script.js
@@ -1,51 +1,75 @@
+// biblioteca de codigos de terceiros
+const formatador = (data) => {
+  return {
+    dia: {
+      numerico: dayjs(data).format('DD'),
+      semana: {
+        curto: dayjs(data).format('ddd'),
+        longo: dayjs(data).format('dddd')
+      }
+    },
+    mes: dayjs(data).format('MMMM'),
+    hora: dayjs(data).format('HH:mm')
+  };
+};
+
+// object {}
 const atividade = {
-  nome: "Almoco",
+  nome: "Almoço",
   data: new Date("2024-07-09 10:00"),
   finalizada: true
-}
+};
 
-
-const atividades = [
-  atividade, 
+// array, lista e vetor
+let atividades = [
+  atividade,
   {
     nome: 'Academia em grupo',
     data: new Date("2024-07-09 12:00"),
-  finalizada: false
+    finalizada: false
   },
   {
     nome: 'Gaming Session',
     data: new Date("2024-07-09 16:00"),
-  finalizada: true
+    finalizada: true
   }
-]
-
-
-
-
+];
 
 const criarItemDeAtividade = (atividade) => {
-  let input = '<input type="checkbox" '
+  let input = '<input type="checkbox" ';
 
   if (atividade.finalizada) {
-    input += 'checked '
+    input += 'checked ';
   }
 
-  input += '>'
+  input += '>';
+
+  const formatar = formatador(atividade.data);
 
   return `
     <div>
       ${input}
       <span>${atividade.nome}</span>
-      <time>${atividade.data}</time>
+      <time>${formatar.dia.semana.longo}, dia ${formatar.dia.numerico} de ${formatar.mes}, às ${formatar.hora}</time>
     </div>
-  `
-}
+  `;
+};
 
-const section = document.querySelector('section')
+const atualizarListaDeAtividades = () => {
+  const section = document.querySelector('section');
 
-for(let atividade of atividades) {
+  // VERIFICAR SE MINHA LISTA ESTÁ VAZIA
+  if (atividades.length == 0) {
+    section.innerHTML = `<p>Nenhuma atividade cadastrada.</p>`;
+    return;
+  } 
 
-section.innerHTML +=  criarItemDeAtividade(atividade)
+  // Limpar o conteúdo da section antes de adicionar os novos elementos
+  section.innerHTML = '';
 
-}
+  for (let atividade of atividades) {
+    section.innerHTML += criarItemDeAtividade(atividade);
+  }
+};
 
+atualizarListaDeAtividades();
